f43ef49a845f7845dfca43ad77a3066c647b0251
Millennium13 <akbarjonmamadaliyev892@gmail.com>
2022-08-18 09:25:11 +0500

    home done

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import { routes } from '../../routes/routes';
 
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+
 export default () => {
   const content = useRoutes(routes);
-  return <>{content}</>;
+  return <><Navbar />{content}<Footer /></>;
 };
